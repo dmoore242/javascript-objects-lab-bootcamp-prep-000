bfbdb64268edc2351c983c1b1b0c243f3330f050
Devin Moore <devin.moore@volken.org>
2017-12-07 03:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({},object);
   delete newObj.key;
-  return object;
-}
-
+  return newObj;
